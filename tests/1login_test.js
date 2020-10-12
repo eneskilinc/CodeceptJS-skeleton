eedce6904804e7f4570ad670a42b02a805ef6c04
Enes KILINÇ <60825534+eneskilinc@users.noreply.github.com>
2020-10-12 17:35:29 +0300

    Update 1login_test.js

--- tests/1login_test.js
+++ tests/1login_test.js
@@ -4,7 +4,7 @@ Scenario('login with a valid datas', ({ I }) => {
     I.amOnPage('/');
     I.click('Sign in', '//*[@id="header"]/div[2]/div/div/nav/div[1]/a');
     I.fillField({id: 'email'}, 'eneskilinch@hotmail.com');
-    I.fillField({id: 'passwd'}, 'eneseness');
+    I.fillField({id: 'passwd'}, 'askpassword');
     I.click('Sign in', {id: 'SubmitLogin'});
     I.seeInTitle('My account - My Store');
 });
